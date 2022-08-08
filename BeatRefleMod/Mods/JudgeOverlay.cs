3eff79339b36bcb452e76c5efbab93c7b96d7b38
i4M1k0SU <i4m1k0su@i4m1k0.su>
2022-08-09 03:51:26 +0900

    fix fonts

--- BeatRefleMod/Mods/JudgeOverlay.cs
+++ BeatRefleMod/Mods/JudgeOverlay.cs
@@ -12,8 +12,6 @@ namespace BeatRefleMod.Mods
         {
             // This function is called once for each instance of this class,
             // when it starts execution (in this case, attached to an object)
-            var appMain = FindObjectOfType<Orgesta.AppMain>();
-            font = appMain.localizeModel.GetFont(Orgesta.AppMain.currentLanguage, false);
         }
 
         public void Update()
@@ -21,6 +19,17 @@ namespace BeatRefleMod.Mods
             // This function is called once per frame, it's frequency depends on the frame rate.
             // This is at the beginning of the game logic cycle.
             scoreManager = FindObjectOfType<ScoreManager>();
+            if (font == null)
+            {
+                // This function is called once per frame, it's frequency depends on the frame rate.
+                // This is at the end of the game logic cycle.
+                var appMain = FindObjectOfType<Orgesta.AppMain>();
+                if (appMain != null)
+                {
+                    font = appMain.localizeModel.GetFont(Orgesta.AppMain.currentLanguage);
+                }
+            }
+            
         }
         public void LateUpdate()
         {
@@ -42,22 +51,27 @@ namespace BeatRefleMod.Mods
             if (font != null)
             {
                 style.font = font;
+                style.fontSize = 30;
             }
-            style.fontStyle = FontStyle.Bold;
-            style.fontSize = 40;
+            else
+            {
+                style.fontStyle = FontStyle.Bold;
+                style.fontSize = 40;
+            }
             int x = 30;
             int y = 170;
             int width = 300;
+            int padding = style.fontSize + 12;
             style.normal.textColor = new Color(24f / 255f, 190f / 255f, 100f / 255f);
             GUI.Label(new Rect(x, y, width, style.fontSize), "COOL\t: " + judgeNotesNum[0].ToString(), style);
             style.normal.textColor = new Color(255f / 255f, 80f /255f, 110f / 255f);
-            GUI.Label(new Rect(x, y += style.fontSize, width, style.fontSize), "GREAT\t: " + judgeNotesNum[1].ToString(), style);
+            GUI.Label(new Rect(x, y += padding, width, style.fontSize), "GREAT\t: " + judgeNotesNum[1].ToString(), style);
             style.normal.textColor = new Color(247f / 255f, 116f / 255f, 63f / 255f);
-            GUI.Label(new Rect(x, y += style.fontSize, width, style.fontSize), "GOOD\t: " + judgeNotesNum[2].ToString(), style);
+            GUI.Label(new Rect(x, y += padding, width, style.fontSize), "GOOD\t: " + judgeNotesNum[2].ToString(), style);
             style.normal.textColor = new Color(41f / 255f, 166f / 255f, 162f / 255f);
-            GUI.Label(new Rect(x, y += style.fontSize, width, style.fontSize), "POOR\t: " + judgeNotesNum[3].ToString(), style);
+            GUI.Label(new Rect(x, y += padding, width, style.fontSize), "POOR\t: " + judgeNotesNum[3].ToString(), style);
             style.normal.textColor = new Color(74f / 255f, 85f / 255f, 90f / 255f);
-            GUI.Label(new Rect(x, y += style.fontSize, width, style.fontSize), "BAD\t: " + judgeNotesNum[4].ToString(), style);
+            GUI.Label(new Rect(x, y += padding, width, style.fontSize), "BAD\t: " + judgeNotesNum[4].ToString(), style);
             switch (scoreManager.Rating)
             {
                 case Orgesta.Define.MusicGame.Rating.E:
@@ -79,9 +93,9 @@ namespace BeatRefleMod.Mods
                     style.normal.textColor = new Color(255f / 255f, 80f / 255f, 110f / 255f);
                     break;
             }
-            GUI.Label(new Rect(x, y += style.fontSize, width, style.fontSize), "RATING\t: " + scoreManager.Rating.ToString(), style);
+            GUI.Label(new Rect(x, y += padding, width, style.fontSize), "RATING\t: " + scoreManager.Rating.ToString(), style);
             style.normal.textColor = Color.white;
-            GUI.Label(new Rect(180, 8, width, style.fontSize), "総ノーツ数 : " + scoreManager.NotesCount.ToString(), style);
+            GUI.Label(new Rect(180, 6, width, style.fontSize), "総ノーツ数 : " + scoreManager.NotesCount.ToString(), style);
         }
 
 
